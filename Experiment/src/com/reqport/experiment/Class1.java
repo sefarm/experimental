5a08fd3e0563e16d042cb89a5b27c180577499de
Steve Farmwald <steve.farmwald@farmwald.net>
2017-03-10 16:23:37 +0100

    dev change 1

---
+++ Experiment/src/com/reqport/experiment/Class1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.reqport.experiment;
+
+/**
+ * @author sef
+ *
+ */
+public class Class1 {
+
+	/**
+	 * 
+	 */
+	public Class1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void doit()
+	{
+		System.out.println(this.getClass().getSimpleName() + ".doit()");
+	}
+}

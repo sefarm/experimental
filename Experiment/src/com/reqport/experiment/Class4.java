773e0cfe4a42a066032be5d5082d979f979f8eaf
Steve Farmwald <steven.farmwald@farmwald.net>
2017-03-23 09:05:58 +0100

    c1

---
+++ Experiment/src/com/reqport/experiment/Class4.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.reqport.experiment;
+
+/**
+ * @author sef
+ *
+ */
+public class Class4 implements IClass {
+
+	/**
+	 * 
+	 */
+	public Class4() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see com.reqport.experiment.IClass#doit()
+	 */
+	@Override
+	public void doit() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
